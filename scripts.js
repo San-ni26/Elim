83de08d1970dc6712dca78bd5d6decf2b0c5e1cf
Kephale <konepassanipaul@gmail.com>
2023-08-09 10:22:59 +0000

    main

--- scripts.js
+++ scripts.js
@@ -1,31 +1,33 @@
-// on recuper tout les element 
-var audio = document.querySelector("audio")
-var track = document.querySelector("track")
-var elapsed = document.querySelector("elapsed")
-var trackTime = document.querySelector("track-time")
-var volumeValue = document.querySelector("volume-value")
-var volume = document.querySelector("volume")
-var pauseBottone = document.querySelector("pause-bottone")
-var playBottone = document.querySelector("play-bottone")
-var stopBottone = document.querySelector("stop-bottone")
-// on recupert la dure du mp3
-let duration = audio.duration;
+    
+    var audio = document.getElementById("audio")
+    var track = document.getElementById("track")
+    var playBottone  = document.getElementById("playBottone")
+    var pauseBottone  = document.getElementById("pauseBottone")
+    var elapsed = document.querySelector("elapsed")
+    var trackTime = document.querySelector("trackTime")
+    playBottone.onclick = function(){
+        if(audio.paused){
+            audio.play();
+            playBottone.src = "icon/pause.svg" 
+        } else{
+            audio.pause();
+            playBottone.src = "icon/play.svg" 
+        } 
+        
+    }
+    pauseBottone.onclick = function(){
+        if(audio.play){
+            audio.pause(); 
+            audio.currentTime = 0;
+            playBottone.src = "icon/play.svg"  
+        }
+    } 
 
-trackTime.textContent = buildDuration(duration);
- 
-// on gere le bottone play
-playBottone.addEventListener("click", function(){
-    pauseBottone.style.display = "initial";
-    stopBottone.style.display = "initial";  
-    this.style.display = "none";   
-})
+    
 
- 
-function buildDuration(duration){
-    let minutes = Math.floor (duration /  60);
-    let reste = duration % 60; 
-    let secondes = Math.floor (reste); 
-    secondes  = string(secondes).padStart (2, "0");
-    return minutes + ":" + secondes;
-}
+    audio.addEventListener("timeupdate", function(){
+        track.value = this.currentTime;
+        elapsed.textContent = buildDuration (this.currentTime);
+    })
+
 
